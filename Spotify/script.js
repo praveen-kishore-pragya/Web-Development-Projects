6f76a9b146c48bdb986cf5935783800b45cefa6b
Praveen Kishore Pragya <72307064+praveen-kishore-pragya@users.noreply.github.com>
2023-02-14 14:43:29 +0530

    Add files via upload

--- Spotify/script.js
+++ Spotify/script.js
@@ -5,6 +5,8 @@ let masterPlay = document.getElementById('masterPlay');
 let songPlayRange = document.getElementById('songPlayRange');
 let gif = document.getElementById("gif");
 let songItems = Array.from(document.getElementsByClassName("songItem"));
+let songIndex=2;
+
 
 let songs = [
     {songName: "Warriyo - Mortals", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
@@ -13,7 +15,7 @@ let songs = [
     {songName: "Different Heaven & EH!DE", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight-feat-Johnning", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"}
 ]
-
+console.log(songs.length)
 
 //setting the cover-image and song name displayed through files in the folder-> covers and songs Array.
 songItems.forEach((element,i) => {
@@ -22,9 +24,33 @@ songItems.forEach((element,i) => {
 });
 
 
+//checking each songList which has been clicked to play
+Array.from(document.getElementsByTagName("svg")).forEach((element)=>{
+    element.addEventListener('click',(e)=>{
+        console.log(e.target.id);
+        songIndex = parseInt(e.target.id);
+        
+        //change song name near seek bar
+        document.getElementById("masterSongName").innerText = songs[songIndex-1].songName;
+
+        if(audioElement.paused || audioElement.currentTime<=0)
+        {
+            audioElement.src=`./songs/${songIndex}.mp3`;
+            audioElement.play();
+            gif.style.opacity = "1";
+        }
+        else
+        {
+            audioElement.pause();
+            gif.style.opacity = "0";
+        }
+    })
+})
 
 
-masterPlay.addEventListener('click',()=>{
+
+masterPlay.addEventListener('click',(e)=>{
+    // console.log(e.target.id);
     if(audioElement.paused || audioElement.currentTime<=0)
     {
         audioElement.play();
@@ -38,20 +64,12 @@ masterPlay.addEventListener('click',()=>{
     }
 })
 
-//checking each songList which has been clicked to play
-Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
-    element.addEventListener('click',(e)={
-        // audioElement.src="./songs/3.mp3";
-
-
-    })
-})
 
 audioElement.addEventListener('timeupdate',()=>{
     progress = parseInt(audioElement.currentTime/audioElement.duration *100);
     // progress was in percentage, so parseInt was used to convert it into integer value
     //Update seekbar
-    console.log(progress);
+    // console.log(progress);
     songPlayRange.value = progress;
     
 })
@@ -62,5 +80,42 @@ songPlayRange.addEventListener('change',()=>{
     // since, here on clicking the seekbar, the current value of progress becomes the currentTime
 })
 
+
+
+//still not working -->previous and next
+document.getElementById("previous").addEventListener('click',()=>{
+    audioElement.pause();
+    gif.style.opacity = "0";
+    // console.log(songIndex);
+    if(parseInt(songIndex)==1)
+        songIndex=songs.length;
+    else
+        songIndex=songIndex-1;
+
+    audioElement.src=`./songs/${songIndex}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    gif.style.opacity = "1";
+})
+
+document.getElementById("next").addEventListener('click',()=>{
+    audioElement.pause();
+    gif.style.opacity = "0";
+    // console.log(songIndex);
+    if(parseInt(songIndex)==songs.length)
+    {
+        songIndex=1;
+    }
+        // songIndex=(songs.length+songIndex)%songs.length;
+    else
+    {
+        songIndex=songIndex+1;
+    }
+
+    audioElement.src=`./songs/${songIndex}.mp3`;
+    audioElement.currentTime=0;
+    audioElement.play();
+    gif.style.opacity = "1";
+})
 
 
